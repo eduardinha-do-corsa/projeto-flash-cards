ff2d409f1f3b9b828f0c9d5c612f8984037ddf15
eduardinha-do-corsa <142987259+eduardinha-do-corsa@users.noreply.github.com>
2024-10-23 07:02:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'UNIVERSO',
+    'O que é uma galáxia?',
+    'Galáxia é um grande conjunto de estrelas, planetas, poeira e gás, mantidos juntos pela gravidade. Nossa galáxia é a Via Láctea.'
+)
+
+criaCartao(
+    'PLANETAS',
+    'Qual o maior planeta do Sistema Solar?',
+    'O maior planeta do sistema solar é Júpiter. Ele tem aproximadamente 139.820 km de diâmetro.'
+)
+
+criaCartao(
+    'SISTEMA SOLAR',
+    'Qual planeta é chamado de "Planeta Vermelho"?',
+    'O Planeta Vermelho é Marte, vizinho da Terra.'
+)
+
+criaCartao(
+    'FILMES',
+    'Qual filme ficou famoso por retratar cientificamente os buracos de minhoca?',
+    'O filme Interstellar (2014) retrata o problema do esgotamento de recursos do Planeta Terra, onde astronautas buscam uma nova chance de recomeçar a vida em outro planeta. Os buracos de minhoca são teoria da relatividade geral de Einstein.'
+)
